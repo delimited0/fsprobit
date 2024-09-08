74d748a8a223e36b9a03e30dc8592cb09881bcb3
Patrick Ding <patrickding00@gmail.com>
2024-09-08 14:35:32 -0400

    small example script

---
+++ tests/getting_started.R
@@ -0,0 +1,56 @@
+library(fsprobit)
+library(mvtnorm)
+
+p = 1
+n_obs = 2000
+n_choices = 4
+
+tol = .001
+conv_metric = "precision"
+relerr_tol = .1
+max_iter = 500
+
+# true parameters ----
+Prec_iden = .2 * diag(n_choices-1) + .8 * rep(1, n_choices-1) %*% t(rep(1, n_choices-1))
+Sigma_iden = solve(Prec_iden)
+
+coef_true = as.matrix(c(2))
+
+# initial parameters
+Sigma_init = diag(n_choices-1)
+coef_init = as.matrix(c(.2))
+
+# covariate mean and sd
+n_mean = 0
+n_sd = 1
+
+simdata = generate_custom_identified_choice_data(
+  n_obs = n_obs,
+  sampler = function(n) rnorm(n, mean=n_mean, sd=n_sd),
+  coef_true = coef_true,
+  Sigma_iden = solve(Prec_iden),
+  seed = 1
+)
+
+# fit model ----
+probit_trace_iden = mnp_probit(
+  X = simdata$X, Y = simdata$Y,
+  beta_init = coef_init,
+  Sigma_init = Sigma_init,
+  E_method = "EP",
+  E_sample_rate = 1,
+  M_method = "Newton",
+  n_choices = n_choices,
+  true_trace = sum(diag(Prec_iden)),
+  tol = tol,
+  newton_tol = 1e-3,
+  max_newton_iter = 50,
+  max_iter = max_iter,
+  conv_metric = conv_metric,
+  shift_iden_method = "ref",
+  scale_iden_method = "trace",
+  verbose=5
+)
+
+# check result ---
+probit_trace_iden$Precision
